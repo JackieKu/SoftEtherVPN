28e1f90206f791d1c1709637ccf117d4cfd3c71d
Kai-Chieh Ku <kjackie@gmail.com>
2016-07-30 23:41:18 +0800

    Separate directory of mutable data with the executables.

--- src/Cedar/Logging.c
+++ src/Cedar/Logging.c
@@ -419,7 +419,7 @@ ERASER *NewEraser(LOG *log, UINT64 min_size)
 
 	e = ZeroMalloc(sizeof(ERASER));
 
-	GetExeDir(dir, sizeof(dir));
+	GetDataDir(dir, sizeof(dir));
 
 	e->Log = log;
 	e->MinFreeSpace = min_size;
